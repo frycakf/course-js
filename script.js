ff870ab87b28b49c9eaf21c81225cfc323792342
frycakf <filip.frycak@gmail.com>
2022-08-26 17:30:32 +0200

    maps

--- script.js
+++ script.js
@@ -409,3 +409,40 @@ const restaurant = {
 // // DOM
 // rest.set(document.querySelector('h1'), 'Heading');
 // console.log(rest);
+
+// Other way od defining and populating Map
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again']
+]);
+console.log(question);
+
+// Converting object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Iterating a map
+console.log(question.get('question'))
+for (const [key, value] of question) {
+  if (typeof key !== 'number') continue;
+
+  console.log(`Answer ${key}: ${value}`);
+}
+
+// QUiz app
+// const answer = Number(prompt('Your answes'));
+//console.log(answer);
+//console.log(question.get('correct'));
+//console.log(question.get(answer === question.get('correct')));
+
+// Convert map to array
+console.log(...question);
+console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
