e51052970046adf759ea4b42892f602e3fb68ff0
frycakf <filip.frycak@gmail.com>
2022-10-01 19:04:50 +0200

    the map() method WIP

--- 11_bankist/script.js
+++ 11_bankist/script.js
@@ -69,13 +69,14 @@ const displayMovements = function (movements) {
 
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
         <div class="movements__value">${mov}</div>
       </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
-
   });
 };
 displayMovements(account1.movements);
@@ -91,5 +92,32 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+// const movementsUSD = [];
+// for (const mov of movements) {
+//   movementsUSD.push(mov * eurToUsd);
+// }
+
+console.log(movements);
+console.log(movementsUSD);
+
+movements.map((mov, i, arr) => {
+  const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+})
 
 /////////////////////////////////////////////////
