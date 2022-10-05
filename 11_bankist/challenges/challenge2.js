5797927bd0f8cfc958d9a1203aa1210743f0849f
frycakf <filip.frycak@gmail.com>
2022-10-05 18:06:50 +0200

    challenge 2 recap

--- 11_bankist/challenges/challenge2.js
+++ 11_bankist/challenges/challenge2.js
@@ -3,6 +3,33 @@
 const data1 = [5, 2, 4, 1, 15, 8, 3];
 const data2 = [16, 6, 10, 5, 6, 1, 4];
 
+const caclAverageHumanAge = function (ages) {
+  const dogAges = ages;
+  console.log(ages);
+
+  // Calculate human ages
+  const humanAges = dogAges.map(dogAge => {
+    return dogAge <= 2 ? dogAge * 2 :16 + dogAge * 4;
+  });
+  console.log(humanAges);
+
+  // Filter age < 18
+  const filterAges = humanAges.filter(function(humanAge) {
+    return humanAge >= 18;
+  })
+  console.log(filterAges);
+
+  // Calc average human age
+  const averageAge = humanAges.reduce(function(acc, el) {
+    return acc + el
+  }, 0) / humanAges.length ;
+  console.log(averageAge);
+}
+
+caclAverageHumanAge(data1)
+
+// === JONAS SOLUTION ===
+
 const calcAverageHumanAge = function(arr) {
   return arr
     .map(function(dogAge) {
