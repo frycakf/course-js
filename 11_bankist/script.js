346b6230a71ee8b39d63da83bbe93e98bb482366
frycakf <filip.frycak@gmail.com>
2022-10-15 19:34:44 +0200

    flat + flatMap method

--- 11_bankist/script.js
+++ 11_bankist/script.js
@@ -170,6 +170,20 @@ btnTransfer.addEventListener('click', function(e) {
   inputTransferAmount.value = inputTransferTo.value = '';
 });
 
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+})
+
 btnClose.addEventListener('click', function(e) {
   e.preventDefault();
 
@@ -194,3 +208,43 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+
+// console.log(movements)
+//
+// // EQUALITY
+// console.log(movements.includes(-130));
+//
+// // CONDITION
+// console.log(movements.some(mov => mov === -130));
+//
+// const anyDeposits = movements.some(mov => mov > 1500);
+// console.log(anyDeposits);
+//
+// console.log(movements.every(mov => mov > 0));
+// console.log(account4.movements.every(mov => mov > 0));
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arr.flat()); // only goes one level deep (by default)
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements)
+//
+// const allMovements = accountMovements.flat();
+// console.log(allMovements)
+//
+// const overalBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+const overallBalance = accounts
+    .map(acc => acc.movements)
+    .flat()
+    .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance)
+
+
+const overallBalance2 = accounts
+    .flatMap(acc => acc.movements)
+    .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2)
