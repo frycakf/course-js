0597409fc6db69236e5d3465e49aac378321efd1
frycakf <filip.frycak@gmail.com>
2022-10-30 14:59:00 +0100

    creating and inserting elements

--- 13_bankist/script.js
+++ 13_bankist/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,14 +19,46 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header'); // 1st element that matches
+const allSections = document.querySelectorAll('.section'); // returns Node list
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button'); // returns HTML collection → is refreshed automatically
+console.log(allButtons);
+
+document.getElementsByClassName('btn'); // returns HTML collection
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); // as the 1st child
+header.append(message); // as the last child
+// header.append(message.cloneNode(true)) // deep = all the child elements will be copied
+
+// header.before(message); // as a sibling
+// header.after(message); // as a sibling
+
+// Delete elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+  // message.parentElement.removeChild(message); // old way
+  message.remove(); // new way
+})
