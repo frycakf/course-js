cb76ace66a979bec1ac0beca63978f2876e5ad98
Filip Fryčák <filip.frycak@gmail.com>
2023-01-08 15:22:35 +0100

    constructor functions and prototypes

--- 14_oop/01_constructorFunctions.js
+++ 14_oop/01_constructorFunctions.js
@@ -1,31 +1,29 @@
+'use strict';
+
 // CONSTRUCTOR FUNCTIONS
-const Person = function (firstName, birthYear) {
-  // Properties
+// 1. New empty object is created
+// 2. Function os called, this = new empty object
+// 3. New object is linked to prototype
+// 4. Function automatically returns the object
+
+const Person  = function (firstName, birthYear)  {
+  // Instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-  // Methods
-  // never do this !!! - bad for performance
+  // Never do this: never create methods inside constructor functions
   this.calcAge = function () {
     console.log(2037 - this.birthYear);
   }
 }
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically returns {}
-
-// we can create as many 'instances' as we want
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
-
-console.log(matilda, jack);
-
-console.log(jonas instanceof Person);
-
 const jay = 'Jay';
+
+console.log(jonas);
+console.log(matilda);
+console.log(jack);
+
 console.log(jay instanceof Person);
--- 14_oop/02_prototypes.js
+++ 14_oop/02_prototypes.js
@@ -1,46 +1,28 @@
-// CONSTRUCTOR FUNCTIONS
-const Person = function (firstName, birthYear) {
-  // Properties
+'use strict';
+
+const Person  = function (firstName, birthYear)  {
   this.firstName = firstName;
   this.birthYear = birthYear;
-
-  // Methods
-  // never do this !!! - bad for performance
-  // by this, we add this method to every created object - this way we create copies
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  }
 }
 
 const jonas = new Person('Jonas', 1991);
-// console.log(jonas);
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically returns {}
-
-// we can create as many 'instances' as we want
 const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
 
-// PROTOTYPES
-console.log(Person.prototype);
-
+// Prototypes
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
-};
+}
 
-jonas.calcAge();
-matilda.calcAge();
 console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype); // true
+console.log(jonas.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(jonas)); // true
+// Person.prototype in NOT prototype of person: proof is below
+console.log(Person.prototype.isPrototypeOf(jonas));
 console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(jonas, matilda);
+console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty('firstName')); // true
-console.log(jonas.hasOwnProperty('species')); // false
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
