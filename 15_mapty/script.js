38cf3df7bcbb2bde818fe73751f5a83629749ef3
Filip Fryčák <filip.frycak@gmail.com>
2023-01-22 16:49:02 +0100

    Leaflet: adding popup on map click

--- 15_mapty/script.js
+++ 15_mapty/script.js
@@ -15,6 +15,33 @@ navigator.geolocation.getCurrentPosition(function (position) {
     const {latitude} = position.coords;
     const {longitude} = position.coords;
     console.log(`https://www.google.cz/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude]
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const {lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+    })
+
 }, function () {
     alert('Could not get your position')
 })
+
