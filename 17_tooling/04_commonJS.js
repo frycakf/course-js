e53b71356f5c61d618d6547aeb01a076acbeefc8
Filip Fryčák <filip.frycak@gmail.com>
2023-02-26 18:29:41 +0100

    commonJS

---
+++ 17_tooling/04_commonJS.js
@@ -0,0 +1,12 @@
+// // ============ CommonJS - used in NodeJS ============
+//
+// // Export
+// export.const addToCart = function (product, quantity) {
+//   cart.push({product, quantity});
+//   console.log(`${quantity} ${product} added to cart`);
+// }
+//
+// // Import
+// const { addTocart } = require('./shoppingCart.js');
+
+// this works only in NodeJS, not in browser!!!!
