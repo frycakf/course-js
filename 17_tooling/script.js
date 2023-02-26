b436c9446c933360c0485d102a145a56b2b68e5d
Filip Fryčák <filip.frycak@gmail.com>
2023-02-26 14:40:30 +0100

    top-level await + module pattern

--- 17_tooling/script.js
+++ 17_tooling/script.js
@@ -1,13 +1,53 @@
-// Importing module
-// import { addToCart, totalPrice as Price, tq } from './shoppingCart.js';
-// addToCart('bread', 5);
-// console.log(Price, tq);
+import * as ShoppingCart from './shoppingCart.js'
+// import {cart} from "./shoppingCart.js";
 
-console.log('Importing module');
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   // console.log(data);
+//
+//   return {title: data.at(-1).title, text: data.at(-1).body}
+// }
+//
+// const lastPost = getLastPost();
+//
+// // Not very clean
+// // lastPost.then(last => console.log(last));
+//
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
 
+const ShoppingCart2 = (function() {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
 
+  const addToCart = function (product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart`);
+  }
 
+  const orderStock = function (product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} ordered from supplier`);
+  }
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity
+  }
+})();
+
+ShoppingCart2.addToCart('apple', 4)
+ShoppingCart2.addToCart('pizza', 2)
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); // = not exported = undefined
--- 17_tooling/shoppingCart.js
+++ 17_tooling/shoppingCart.js
@@ -1,15 +1,19 @@
-// Exporting module
-console.log('Exporting module');
-
-const shippingCost = 10;
-export const cart = [];
-
-export const addToCart = function (product, quantity) {
-  cart.push({product, quantity});
-  console.log(`${quantity} ${product} added to cart`);
-}
-
-const totalPrice = 237;
-const totalQuantity = 23;
-
-export { totalPrice, totalQuantity as tq };
+// // Exporting module
+// console.log('Exporting module');
+//
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished fetching users');
+//
+// const shippingCost = 10;
+// export const cart = [];
+//
+// export const addToCart = function (product, quantity) {
+//   cart.push({product, quantity});
+//   console.log(`${quantity} ${product} added to cart`);
+// }
+//
+// const totalPrice = 237;
+// const totalQuantity = 23;
+//
+// export { totalPrice, totalQuantity as tq };
