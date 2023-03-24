fe42b194bd076173f0e497b2c97cc39d7af42d1c
Filip Fryčák <filip.frycak@gmail.com>
2023-03-24 21:06:40 +0100

    rendering bookmarks

--- 18_forkify/src/js/controller.js
+++ 18_forkify/src/js/controller.js
@@ -6,10 +6,11 @@ import recipeView from "./views/recipeView";
 import searchView from "./views/searchView";
 import resultsView from "./views/resultsView";
 import paginationView from "./views/paginationView";
+import bookmarksView from "./views/bookmarksView";
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -27,6 +28,7 @@ const controlRecipes = async function() {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage())
+    bookmarksView.update(model.state.bookmarks)
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -80,10 +82,15 @@ const controlServings = function(newServings) {
 }
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe)
   else model.deleteBookmark(model.state.recipe.id)
 
+  // 2) Update recipe view
   recipeView.update(model.state.recipe)
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks)
 }
 
 const init = function () {
